f01faeea0d93b7d85ae8283726050ab5384ba774
dranilew <andrewhanzhuoli@gmail.com>
2025-08-15 04:09:13 -0700

    Add temporary shell script for installation while I figure out other options

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Run from base of repository.
+go build ./src/cmd/manager
+go build ./src/cmd/mcctl
+
+cp minecraft-server-manager.service /usr/lib/systemd/system/
+systemctl daemon-reload
+
+systemctl stop minecraft-server-manager.service
+cp manager /usr/bin/minecraft-server-manager
+systemctl enable --now minecraft-server-manager.service
+
+cp mcctl /usr/bin/

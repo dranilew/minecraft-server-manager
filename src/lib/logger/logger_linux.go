5c84437c242044a79499b0c59906c364c913224f
dranilew <andrewhanzhuoli@gmail.com>
2025-08-17 08:49:08 -0700

    Remove stderr logger

--- src/lib/logger/logger_linux.go
+++ src/lib/logger/logger_linux.go
@@ -15,8 +15,7 @@ func initPlatformLogger(tag string) error {
 	}
 	syslogLogger := log.New(syslogWriter, "", 0)
 	stdoutLogger := log.New(os.Stdout, "", 0)
-	stderrLogger := log.New(os.Stderr, "", 0)
 
-	loggers = []*log.Logger{syslogLogger, stdoutLogger, stderrLogger}
+	loggers = []*log.Logger{syslogLogger, stdoutLogger}
 	return nil
 }
